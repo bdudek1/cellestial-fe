359a10e8239e3f0db5920e2f7d0dd3a98a8871e2
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2024-03-03 10:57:41 +0100

    Fix UI bugs

--- src/visibility/Visibility.tsx
+++ src/visibility/Visibility.tsx
@@ -13,23 +13,17 @@ export const Visibility = () => {
   const [position, setPosition] = useState<Position>({ lat: 52, lng: 21 });
   const [dateTime, setDateTime] = useState<Value>(new Date());
 
-  useEffect(() => {
-    console.log('DATE TIME', new Date(dateTime?.toString() ?? '').getTime());
-    console.log('LAT', position.lat);
-    console.log('LNG', position.lng);
-  }, [dateTime, position.lat, position.lng]);
-
   return (
     <Container fluid>
       <StyledHeader>Select place and time of planned star gazing!</StyledHeader>
       <Map position={position} setPosition={setPosition} />
-      <Row className="justify-content-end mt-3">
+      <Row className="justify-content-end mt-3 p-1">
         <Col xxl={7} lg={6} sm={1} xs={0} />
-        <Col xxl={3} lg={4} sm={7} xs={8}>
+        <Col xxl={3} lg={4} sm={7} xs={12}>
           <DateTimeSelector value={dateTime} onChange={setDateTime} />
         </Col>
-        <Col xxl={2} lg={2} sm={4} xs={4}>
-          <Button style={{ marginTop: '0.5rem' }}>Get Cellestials!</Button>
+        <Col xxl={2} lg={2} sm={4} xs={12}>
+          <Button style={{ marginTop: '0.5rem' }}>Get Celestial Weather!</Button>
         </Col>
       </Row>
     </Container>
